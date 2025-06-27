610b0b25360489841378814cfd9ba5bc7b37c63b
Abhishek jaiswar <abhishekndxw@gmail.com>
2025-06-27 18:04:28 +0530

    03-trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
 
-const Page = () => {
+import React, { Suspense } from 'react'
+import { getQueryClient, trpc } from '@/trpc/server'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Client } from './client';
+
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "Abhishek @@PREFETCHED" }))
   return (
-    <div>
-      <h1 className="font-bold text-rose-400">Hello</h1>
-      <Button variant={'vermount'}>Are you ready?</Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading.....</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
-export default Page
+export default Page 
